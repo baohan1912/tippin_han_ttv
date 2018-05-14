48c3037299440bbf37bba94227835e0fe6aef81c
Tôn Thất Bảo Hân (TTV) <han-ttb-ttv@fujinet.vn>
2018-05-14 16:15:00 +0700

    Initial commit

---
+++ app/src/test/java/com/example/han_ttb_ttv/tippin_han_ttv/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.han_ttb_ttv.tippin_han_ttv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
